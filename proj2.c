809636d0e328a6635e85d36254549fdebb710a70
jakubKuznik <kuznja16@gmail.com>
2021-04-15 11:38:43 +0200

    Argument parser done.

--- proj2.c
+++ proj2.c
@@ -3,6 +3,9 @@
 // Author:      Jakub Kuzník, FIT
 // Compiled:    gcc 9.9.3.0
 
+/* start 
+    ./proj2 NE NR TE TR 
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,8 +13,8 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-
 #include "proj2.h"
+
 
 int main(int argc, char *argv[])
 {
@@ -28,36 +31,65 @@ int main(int argc, char *argv[])
 
 /**
  * Parse arguments. 
- * return -1 error bad file format 
- * return -2 if there are no args 
+ * return -1 if there is error  
  */
-long int arg_parser(int argc, char *argv[], short *ne, short *nr, short *te, short *tr)
+int arg_parser(int argc, char *argv[], short *ne, short *nr, short *te, short *tr)
 {
     if (argc != 5) 
         goto error_1; //bad input arguments 
 
     // Check if input argument are numbers 
     for(int i = 1; i < argc; i++)
-        if(is_number(argv[i]) == false)
+        if(is_number(argv[i]) == false) // Check if argv[i] is regular number 
             goto error_2;
 
+    // store args to given variables and chceck if it is in good range if not go to error3 
+    //NE
+    if(store_if_gt_lt(argv[NE], ne, MIN_NE_NR, MAX_NE) == false) 
+        goto error_3;
+    //NR
+    if(store_if_gt_lt(argv[NR], nr, MIN_NE_NR, MAX_NR) == false) 
+        goto error_3;
+    //TE
+    if(store_if_gt_lt(argv[TE], te, MIN_TE_RE, MAX_TE_RE) == false) 
+        goto error_3;
+    //TR
+    if(store_if_gt_lt(argv[TR], tr, MIN_TE_RE, MAX_TE_RE) == false) 
+        goto error_3;
 
     return 0;
 
-
-
-//////////////ERRORS
+//ERRORS
+// bad argument format 
 error_1:
-    fprintf(stderr, "Bad input argumetns format.\n");
+    fprintf(stderr, "ERROR: Bad input argumetns format.\n");
     return -1;
+// No number input 
 error_2:
-    fprintf(stderr, "Wrong input these are no numbers. \n");
+    fprintf(stderr, "ERROR: Wrong input these are no numbers. \n");
     return -1;
-
-/*
+// To big or to small number 
 error_3:
+    fprintf(stderr, "ERROR: Number is to small or to big. \n");
     return -1;
+}
+
+
+/**
+ * Store char number to if lt and gt conditons are meet 
+ * else return false 
+ * <range_from, range_to>
 */
+bool store_if_gt_lt(char *from, short *to, int range_from, int range_to)
+{
+    short temp = atoi(from);
+    if(temp < range_from)
+        return false;
+    else if(temp > range_to)
+        return false;
+
+    *to = temp;
+    return true;
 }
 
 /**
--- proj2.h
+++ proj2.h
@@ -5,14 +5,37 @@
 // header file for proj2.c
 
 
+
+// Postion in arguments 
+#define NE 1
+#define NR 2
+#define TE 3
+#define TR 4
+
+//Maximum argument size for variable 
+#define MAX_NE 999
+#define MAX_TE_RE 1000
+#define MAX_NR 19
+
+//Argument minimum
+#define MIN_NE_NR 1
+#define MIN_TE_RE 0
+
+
 /**
  * Parse arguments. 
  * return -1 error 
  * return -2 if there are no args 
  */
-long int arg_parser(int argc, char *argv[], short *ne, short *nr, short *te, short *tr);
+int arg_parser(int argc, char *argv[], short *ne, short *nr, short *te, short *tr);
 
 
+/**
+ * Store char number to if lt and gt conditons are meet 
+ * else return false 
+ * <range_from, range_to>
+*/
+bool store_if_gt_lt(char *from, short *to, int range_from, int range_to);
 
 /**
  * Return true if string is digit, else return false.
