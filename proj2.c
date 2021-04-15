990f7eafd42961edb50ee192f2847e83ff0d0bdd
jakubKuznik <kuznja16@gmail.com>
2021-04-15 09:28:57 +0200

    Creating files hiearchy.

---
+++ proj2.c
@@ -0,0 +1,12 @@
+// Solution for IOS-proj2, 15.4.2021
+// File:        proj2.c
+// Author:      Jakub Kuzník, FIT
+// Compiled:    gcc 9.9.3.0
+
+#include <stdio.h>
+#include "proj2.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ proj2.h
@@ -0,0 +1,6 @@
+// Solution for IOS-proj2, 15.4.2021
+// File:        proj2.h
+// Author:      Jakub Kuzník, FIT
+// Compiled:    gcc 9.9.3.0
+
+
